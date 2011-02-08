50004d58d893a3e38c06c04d266de5d674213f3d
Christopher Swasey <christopher.swasey@gmail.com>
2011-02-08 02:56:28 -0500

    FixedHeader is been refactored and seems to work great

--- mixins/fixed_header.js
+++ mixins/fixed_header.js
@@ -7,77 +7,126 @@
 
 Endash.FixedListHeaders = {
 
-	reloadIfNeeded: function() {
-		var invalid = this._invalidIndexes;
-		if (!invalid || !this.get('isVisibleInWindow'))
-			var skipHeader = true
-
-		var ret = sc_super()
+  reloadIfNeeded: function() {
+		var skipHeader, nowShowing, ret, header, group
 		
-		if(!skipHeader)
-			this.setFixedHeader()
+    var invalid = this._invalidIndexes;
+    if (!invalid || !this.get('isVisibleInWindow') || !this.get('content'))
+			skipHeader = true
 
+		ret = sc_super()
+		
+		if(skipHeader)
+			return ret
+		
+		this.layoutHeader()
+
+    return ret
+  },
+
+	layoutHeader: function() {
+		// the current group is ALWAYS the first index in nowShowing
+		// there are three possibilities:
+		// the second showing item (third index) is not a group
+		// the second showing item is a group and we're scrolling downward
+		// the second showing item is a group and we're scrolling upward
+
+		var nowShowing = this.get('nowShowing'),
+			group = nowShowing.get('min'),
+			nowShowing = nowShowing.toArray(),
+			groups = this.get('_contentGroupIndexes').toArray(),
+			secondItem = nowShowing.objectAt(1),
+			thirdItem = nowShowing.objectAt(2),
+			oldGroup = this._group,
+			oldHeader, currentHeader, layout, layout2
+
+		// the second item being displayed is a group
+
+		if(groups.contains(thirdItem)) {
+
+			if(thirdItem > oldGroup) {
+				// we're scrolling down
+				
+				oldHeader = this.itemViewForContentIndex(group)
+				layout = this.layoutForContentIndex(secondItem)
+				
+				oldHeader.$().css('position', 'absolute')
+				oldHeader.adjust(layout)
+			} else {				
+				// we're scrolling up
+				
+				oldHeader = this.itemViewForContentIndex(oldGroup)
+				layout = this.layoutForContentIndex(oldGroup)
+				
+				currentHeader = this.itemViewForContentIndex(group)				
+				layout2 = this.layoutForContentIndex(secondItem)
+				
+				oldHeader.$().css(layout).css('position', 'absolute').css('z-index', 5000)
+				oldHeader.adjust(layout)
+				currentHeader.adjust(layout2)
+			}
+			
+		} else {
+			
+			if((group !== oldGroup) || this._layoutHeader) {
+				header = this.itemViewForContentIndex(group),
+					scrollView = this.getPath('parentView.parentView'),
+			  	offset = scrollView.$().offset()
+		
+				if(!header.get('layer'))
+					this._layoutHeader = true
+				else {
+					this._layoutHeader = false
+
+					if(oldGroup && nowShowing.contains(oldGroup)) {
+						oldHeader = this.itemViewForContentIndex(oldGroup)
+				
+						oldHeader.adjust(this.layoutForContentIndex(this._group))
+						oldHeader.$().css('position', 'absolute')
+					}
+				
+					this._group = group
+			
+					var layout = {top: offset.top + 1, left: offset.left + 1, width: this.get('frame').width}
+			
+					header.$().css(layout).css('position', 'fixed').css('z-index', '5000')
+					header.adjust(layout)
+				}
+
+			}
+		}
+	},
+
+	contentIndexesInRect: function(rect) {
+		var ret = sc_super()
+		var group = this.currentGroup(ret.get('min'))
+		
+		if(group !== false)
+			if(!ret.contains(group))
+				ret.add(group)
+				
 		return ret
 	},
-	
-	setFixedHeader: function() {
-		// console.log('setFixedHeader')
+  
+  currentGroup: function(idx) {
 		if(!this.get('content'))
-			return
+			return false
 			
-		var header, offset, content, idx,
-			content, item, scrollView,
-			nowShowing = this.get('nowShowing'),
-			idx = nowShowing.get('min'),
-			groups = this.get('_contentGroupIndexes'),
-			isGroup = groups.contains(idx),
-			content = this.get('content'),
-			item = content.objectAt(idx)
-			
-		if(idx === this._fixedHeaderIdx)
-		 	return
+     var groups = this.get('_contentGroupIndexes').toArray(),
+			len = groups.get('length'),
+			last, i
+
+		if(groups.contains(idx))
+			return idx
 		
-		if(!isGroup) {
-		 	if(item.get('parentItem') == content.objectAt(this._fixedHeaderIdx))
-				return
-			else 
-				idx = content.indexOf(item.get('parentItem'))
+		for(i = 0; i < len; i++) {
+			if(groups.objectAt(i) > idx)
+				return last
+			last = groups.objectAt(i)
 		}
 		
-		if(this._fixedHeader)
-			this._fixedHeader.destroy()
-
-		header = this.itemViewForContentIndex(idx)
-
-		delete this._sc_itemViews[idx]
-		
-		scrollView = this.getPath('parentView.parentView')
-		offset = scrollView.$().offset()
-
-		header.$().css('position', 'fixed').css('z-index', '5000').css('top', offset.top).css('left', offset.left + 1)
-			.css('right', null).css('width', this.get('frame').width + 1)
-			
-		header.set('layerId', this.get('layerId') + '-header')
-
-		this._fixedHeader = header
-		this._fixedHeaderIdx = idx
-	},
-	
-	layoutForContentIndex: function(idx) {
-		var view = this.itemViewForContentIndex(idx)
-		if(view.isGroup && view == this.get('currentGroup'))
-			
-		else
-			return sc_super()
-	},
-	
-	currentGroup: function() {
-		
-	}.property('nowShowing').cacheable(),
-	
-	frameDidChange: function() {
-		this.setFixedHeader()
-	}.observes('frame')
-	
+		return last
+  }
+  
 }
-	
+  
