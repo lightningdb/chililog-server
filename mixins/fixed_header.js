f5913927ea3afca20a3f5bbb43440aa1dea44d0c
Christopher Swasey <christopher.swasey@gmail.com>
2011-02-08 23:31:28 -0500

    Added comments, removed some extraneus code, primary manual css modification

--- mixins/fixed_header.js
+++ mixins/fixed_header.js
@@ -5,6 +5,9 @@
 // obviously, this should only be used within a scroll view
 // and for simplicity's sake, *not* a nested scrollview
 
+// (c) 2011 Christopher Swasey (Endash)
+// christopher.swasey@gmail.com
+
 Endash.FixedListHeaders = {
 
   reloadIfNeeded: function(nowShowing, scrollOnly) {
@@ -52,22 +55,30 @@ Endash.FixedListHeaders = {
       scrollView = this.getPath('parentView.parentView'),
       oldHeader, currentHeader, layout, layout2, header, offset;
 
-    // the second item being displayed is a group
+    // If the second item being displayed is a group,
+    // then we position the two headers 'normally'
+    // to allowing them to scroll into position/off the screen
+    // note that the second item in the display is the
+    // third item in the nowShowing array
 
     if(groups.contains(thirdItem)) {
 
       if(thirdItem > oldGroup) {
-        // we're scrolling down
+        // we're scrolling down, so the next group is moving upward
+        // visually speaking. we position the current header
+        // over the last item in its group, so that the new header
+        // appears to be pushing it out of the way
 
         oldHeader = this.itemViewForContentIndex(group);
         layout = this.layoutForContentIndex(secondItem);
 
-        oldHeader.$().css('position', 'absolute');
         oldHeader.adjust(layout);
 
         this.get('layer').appendChild(oldHeader.get('layer'));
       } else {
-        // we're scrolling up
+        // we're scrolling up, so the next header is moving downward
+        // visually. We reposition the old header to its 'proper'
+        // position
 
         oldHeader = this.itemViewForContentIndex(oldGroup);
         layout = this.layoutForContentIndex(oldGroup);
@@ -75,7 +86,6 @@ Endash.FixedListHeaders = {
         currentHeader = this.itemViewForContentIndex(group);
         layout2 = this.layoutForContentIndex(secondItem);
 
-        oldHeader.$().css(layout).css('position', 'absolute').css('z-index', 5000);
         oldHeader.adjust(layout);
         currentHeader.adjust(layout2);
 
@@ -84,25 +94,25 @@ Endash.FixedListHeaders = {
 
       this._layoutHeader = true;
     } else {
+
+      // Either the group has changed, or the current header
+      // needs to be laid out
+
       if((group !== oldGroup) || this._layoutHeader) {
         header = this.itemViewForContentIndex(group);
 
         if(!header.get('layer')) {
+          // the header will be laid out next time since
+          // we don't have the DOM element yet
+
           this._layoutHeader = true;
         } else {
           this._layoutHeader = false;
-
-          if(oldGroup && nowShowing.contains(oldGroup)) {
-            oldHeader = this.itemViewForContentIndex(oldGroup);
-
-            oldHeader.adjust(this.layoutForContentIndex(this._group));
-            oldHeader.$().css('position', 'absolute');
-          }
-
           this._group = group;
 
+          // Move the header to the scrollView, so we can position it
+          // and set the layout 
           layout = {top: 0, left: 0, width: this.get('frame').width};
-          header.$().css(layout).css('z-index', '5000').addClass('sc-group-item');
           scrollView.get('layer').appendChild(header.get('layer'));
           header.adjust(layout);
         }
@@ -110,6 +120,8 @@ Endash.FixedListHeaders = {
       }
     }
   },
+
+  // Put the current group at the beginning of the nowShwowing
 
   contentIndexesInRect: function(rect) {
     var ret = sc_super();
@@ -123,6 +135,10 @@ Endash.FixedListHeaders = {
 
     return ret;
   },
+  
+  // Get the current group, by stepping through the groups
+  // and getting the largest groupId without going over
+  // the first index showing
 
   currentGroup: function(idx) {
     if(!this.get('content')) {
