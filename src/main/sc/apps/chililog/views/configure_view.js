e28b7c135109075838d0b086fc61de5fa2bd1b01
Veebs <vibuli@gmail.com>
2011-04-20 22:41:12 +1000

    SC- added splitview to configure

--- src/main/sc/apps/chililog/views/configure_view.js
+++ src/main/sc/apps/chililog/views/configure_view.js
@@ -9,7 +9,7 @@
 
 Chililog.configureView = SC.View.design({
   layout: { top: 10, left: 10, bottom: 10, right: 10 },
-  childViews: 'title tree'.w(),
+  childViews: 'title body'.w(),
 
   title: SC.LabelView.design({
     layout: { top: 0, left: 0, width: 200, height: 30 },
@@ -19,12 +19,29 @@ Chililog.configureView = SC.View.design({
     localize: YES
   }),
 
-  tree: SC.ListView.design({
-    layout: { top: 35, left: 0, bottom: 10, width: 400 },
-    contentValueKey: 'treeItemName',
-    contentBinding: 'Chililog.configureViewTreeController.arrangedObjects',
-    selectionBinding: 'Chililog.configureViewTreeController.selection'
-  })  //tree
+  body: SC.SplitView.design({
+    layout: { top: 35, left: 0, right: 0, bottom: 0 },
+    classNames: ['edit-box'],
+    defaultThickness: 0.2,
+
+    topLeftView: SC.ScrollView.design({
+      layout: { top: 0, bottom: 0, left: 0, right: 0 },
+      hasHorizontalScroller: NO,
+
+      contentView: SC.ListView.design({
+        layout: { top: 0, bottom: 0, left: 0, right: 0 },
+        rowHeight: 24,
+        contentValueKey: 'treeItemName',
+        contentBinding: 'Chililog.configureViewTreeController.arrangedObjects',
+        selectionBinding: 'Chililog.configureViewTreeController.selection'
+      })
+    }),
+
+    bottomRightView: SC.View.design({
+    })
+  })
 
 
+
+  
 });
