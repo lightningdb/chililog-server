bb2125650e0256e000f9ff1b936a58eb38a0a7b5
Veebs <vibuli@gmail.com>
2011-08-22 19:38:24 +1000

    Removed compiler warning

--- src/test/java/org/chililog/server/workbench/WorkbenchServiceTest.java
+++ src/test/java/org/chililog/server/workbench/WorkbenchServiceTest.java
@@ -28,7 +28,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.text.ParseException;
