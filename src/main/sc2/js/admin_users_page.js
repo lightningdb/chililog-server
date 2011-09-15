364f395dbfdf24b7d265af488d5b1fff87c53db0
Veebs <vibuli@gmail.com>
2011-09-16 09:26:11 +1000

    Basic user dialog working

--- src/main/sc2/js/admin_users_page.js
+++ src/main/sc2/js/admin_users_page.js
@@ -117,7 +117,25 @@ App.Results = SC.View.extend({
   currentStatusLabel: '_admin.user.currentStatus'.loc(),
 
   CollectionView : SC.CollectionView.extend({
-    contentBinding: 'App.resultsController'
+    contentBinding: 'App.resultsController',
+
+    itemViewClass: SC.View.extend({
+      tagName: 'tr',
+
+      // Spit out the content's index in the array proxy as an attribute of the tr element
+      attributeBindings: ['contentIndex'],
+
+      willInsertElement: function() {
+        this._super();
+
+        // Add handler for double clicking
+        var id = this.$().attr('id');
+        this.$().dblclick(function() {
+          App.statechart.sendAction('showDialog', $('#' + this.id).attr('contentIndex'));
+        });
+      }
+    })
+    
   })
 });
 
@@ -150,6 +168,236 @@ App.WorkingImage = App.ImgView.extend({
   isVisibleBinding: SC.Binding.from('App.pageController.isSearching').oneWay().bool()
 });
 
+
+
+/**
+ * @class
+ * Dialog div
+ */
+App.Dialog = SC.View.extend({
+  attributeBindings: ['title'],
+
+  title: '_admin.user.editTitle'.loc(),
+
+  didInsertElement: function() {
+    this._super();
+
+    // JQuery UI dialog setup
+    this.$().dialog({
+      autoOpen: false,
+      height: 400,
+      width: 850,
+      modal: true,
+      close: function(event, ui) {
+        // For when the X is clicked
+        App.statechart.sendAction('hideDialog');
+      }
+    });
+
+    // Add event handler to tab <a>. Delegate does  it for current and future tabs
+    this.$().delegate('ul.tabs li > a', 'click', function(e) {
+      var $this = $(this),
+        href = $this.attr('href'),
+        li = $this.parent('li'),
+        ul = li.parent();
+
+      if (/^#\w+/.test(href)) {
+        e.preventDefault();
+      }
+
+      if ($this.hasClass('active')) {
+        return
+      }
+
+      var $href = $(href)
+
+      // Make this li active
+      ul.find('.active').removeClass('active')
+      li.addClass('active')
+
+      // Make linked fieldset active
+      $href.parent().find('.active').removeClass('active')
+      $href.addClass('active')
+    });
+  }
+
+});
+
+/**
+ * @class
+ * Common functions for field data
+ */
+App.DialogFieldDataMixin = {
+
+  // Hide dialog on ENTER key pressed
+  insertNewline: function() {
+    App.statechart.sendAction('hideDialog');
+    return;
+  },
+
+  // Hide dialog on ESC key pressed
+  cancel: function() {
+    App.statechart.sendAction('hideDialog');
+    return;
+  }
+};
+
+/**
+ * @class
+ * Name Field
+ */
+App.DialogUserNameField = App.FieldView.extend({
+  label: '_admin.user.username'.loc(),
+  help: '_admin.user.username.help'.loc(),
+  DataView : App.TextBoxView.extend(App.DialogFieldDataMixin, {
+    classNames: 'medium'.w(),
+    valueBinding: 'App.pageController.selectedRecord.username'
+  })
+});
+
+/**
+ * @class
+ * Display Name Field
+ */
+App.DialogDisplayNameField = App.FieldView.extend({
+  label: '_admin.user.displayName'.loc(),
+  DataView : App.TextBoxView.extend(App.DialogFieldDataMixin, {
+    classNames: 'xlarge'.w(),
+    valueBinding: 'App.pageController.selectedRecord.displayName'
+  })
+});
+
+/**
+ * @class
+ * Email Address Field
+ */
+App.DialogEmailAddressField = App.FieldView.extend({
+  label: '_admin.user.emailAddress'.loc(),
+  DataView : App.TextBoxView.extend(App.DialogFieldDataMixin, {
+    classNames: 'xlarge'.w(),
+    valueBinding: 'App.pageController.selectedRecord.emailAddress'
+  })
+});
+
+/**
+ * @class
+ * Status Field
+ */
+App.DialogStatusField = App.FieldView.extend({
+  label: '_admin.user.currentStatus'.loc(),
+  DataView : App.SelectView.extend(App.DialogFieldDataMixin, {
+    classNames: 'xlarge'.w(),
+    contentBinding: 'App.pageController.currentStatusOptions',
+    valueBinding: SC.Binding.from('App.pageController.selectedRecord.currentStatus').transform(function(value, isForward) {
+      if (isForward) {
+        var options = App.pageController.get('currentStatusOptions');
+        for (var i=0; i< options.length; i++) {
+          if (options[i].get('value') === value) {
+            return options[i];
+          }
+        }
+        return options[0];
+      } else {
+        return value.get('value');
+      }
+    })
+  }),
+  isVisibleBinding: SC.Binding.from('App.pageController.isNewSelectedRecord').oneWay().bool().not()
+});
+
+/**
+ * @class
+ * Email Address Field
+ */
+App.DialogPasswordField = App.FieldView.extend({
+  label: '_admin.user.password'.loc(),
+  DataView : App.TextBoxView.extend(App.DialogFieldDataMixin, {
+    classNames: 'large'.w(),
+    valueBinding: 'App.pageController.selectedRecord.password'
+  }),
+  isVisibleBinding: SC.Binding.from('App.pageController.isNewSelectedRecord').oneWay().bool()
+});
+
+/**
+ * @class
+ * Email Address Field
+ */
+App.DialogConfirmPasswordField = App.FieldView.extend({
+  label: '_admin.user.confirmPassword'.loc(),
+  DataView : App.TextBoxView.extend(App.DialogFieldDataMixin, {
+    classNames: 'large'.w(),
+    valueBinding: 'App.pageController.confirmPassword'
+  }),
+  isVisibleBinding: SC.Binding.from('App.pageController.isNewSelectedRecord').oneWay().bool()
+});
+
+/**
+ * @class
+ * Status Field
+ */
+App.DialogIsSystemAdministratorField = App.FieldView.extend({
+  label: '_admin.user.isSystemAdministrator'.loc(),
+  DataView : App.SelectView.extend(App.DialogFieldDataMixin, {
+    classNames: 'xxlarge'.w(),
+    contentBinding: 'App.pageController.isSystemAdministratorOptions',
+    valueBinding: SC.Binding.from('App.pageController.selectedRecord.isSystemAdministrator').transform(function(value, isForward) {
+      if (isForward) {
+        var options = App.pageController.get('isSystemAdministratorOptions');
+        for (var i=0; i< options.length; i++) {
+          if (options[i].get('value') === value) {
+            return options[i];
+          }
+        }
+        return options[0];
+      } else {
+        return value.get('value');
+      }
+    })
+  })
+});
+
+/**
+ * @class
+ * Button to show previous log entry
+ */
+App.DialogPreviousButton = App.ButtonView.extend({
+  label: '_previous'.loc(),
+  title: '_previousTooltip'.loc(),
+  disabledBinding: SC.Binding.from('App.pageController.canShowPreviousRecord').oneWay().bool().not(),
+
+  click: function() {
+    App.statechart.sendAction('showPreviousRecord');
+    return;
+  }
+});
+
+/**
+ * @class
+ * Button to show next log entry
+ */
+App.DialogNextButton = App.ButtonView.extend({
+  label: '_next'.loc(),
+  title: '_nextTooltip'.loc(),
+  disabledBinding: SC.Binding.from('App.pageController.canShowNextRecord').oneWay().bool().not(),
+
+  click: function() {
+    App.statechart.sendAction('showNextRecord');
+    return;
+  }
+});
+
+/**
+ * @class
+ * Button to close dialog window
+ */
+App.DialogDoneButton = App.ButtonView.extend({
+  label: '_done'.loc(),
+
+  click: function() {
+    App.statechart.sendAction('hideDialog');
+    return;
+  }
+});
 
 // --------------------------------------------------------------------------------------------------------------------
 // Controllers
@@ -221,7 +469,109 @@ App.pageController = SC.Object.create({
    *
    * @type Object
    */
-  previousSearchCriteria: null
+  previousSearchCriteria: null,
+
+  /**
+   * Options for user's status
+   * @type Array of SC.Object
+   */
+  currentStatusOptions: [
+    SC.Object.create({label: '_admin.user.currentStatus.enabled'.loc(), value: 'ENABLED'}),
+    SC.Object.create({label: '_admin.user.currentStatus.disabled'.loc(), value: 'DISABLED'}),
+    SC.Object.create({label: '_admin.user.currentStatus.locked'.loc(), value: 'LOCKED'})
+  ],
+
+  /**
+   * Options for user's system admin role
+   * @type Array of SC.Object
+   */
+  isSystemAdministratorOptions: [
+    SC.Object.create({label: '_admin.user.isSystemAdministrator.yes'.loc(), value: YES}),
+    SC.Object.create({label: '_admin.user.isSystemAdministrator.no'.loc(), value: NO})
+  ],
+
+  /**
+   * Index of selected record in App.resultsController
+   *
+   * @type int
+   */
+  selectedRecordIndex: -1,
+
+  /**
+   * Selected record to display in the dialog
+   *
+   * @type App.RepositoryConfigRecord
+   */
+  selectedRecord: null,
+
+  /**
+   * Flag to indicate if the selected record is a new record
+   */
+  isNewSelectedRecord: function() {
+    var docID = this.getPath('selectedRecord.documentID');
+    return SC.empty(docID);
+  }.property('selectedRecord.documentID').cacheable(),
+
+  /**
+   * Select the record specified by the index
+   * @param {int} recordIndex index of the selected record in App.resultsController
+   */
+  selectRecord: function(recordIndex) {
+    var record = App.resultsController.objectAtContent(recordIndex);
+    var nestedRecord = App.userEngine.edit(record.get(App.DOCUMENT_ID_RECORD_FIELD_NAME));
+    App.pageController.set('selectedRecordIndex', recordIndex);
+    App.pageController.set('selectedRecord', nestedRecord);
+  },
+
+  /**
+   * Deselect the record specified by the index
+   */
+  deselectRecord: function() {
+    var nestedRecord = App.pageController.get('selectedRecord');
+    App.userEngine.discardChanges(nestedRecord);
+
+    App.pageController.set('selectedRecordIndex', -1);
+    App.pageController.set('selectedRecord', null);
+  },
+
+  /**
+   * Flag to indicate if we can show the previous button
+   *
+   * @type Boolean
+   */
+  canShowPreviousRecord: function() {
+    var selectedRecordIndex = App.pageController.get('selectedRecordIndex');
+    if (selectedRecordIndex <= 0) {
+      return NO;
+    }
+
+    var recordStatus = this.getPath('authenticatedUserRecord.status');
+     if (!SC.none(recordStatus) && recordStatus !== SC.Record.READY_CLEAN && !this.get('isSaving')) {
+      return NO;
+    }
+
+    return YES;
+  }.property('selectedRecordIndex', '*selectedRecord.status').cacheable(),
+
+  /**
+   * Flag to indicate if we can show the next button
+   *
+   * @type Boolean
+   */
+  canShowNextRecord: function() {
+    var selectedRecordIndex = App.pageController.get('selectedRecordIndex');
+    if (selectedRecordIndex === App.resultsController.get('length') - 1) {
+      return NO;
+    }
+
+    var recordStatus = this.getPath('authenticatedUserRecord.status');
+     if (!SC.none(recordStatus) && recordStatus !== SC.Record.READY_CLEAN && !this.get('isSaving')) {
+      return NO;
+    }
+
+    return YES;
+  }.property('selectedRecordIndex', '*selectedRecord.status').cacheable()
+
 });
 
 /**
@@ -259,6 +609,55 @@ App.statechart = SC.Statechart.create({
       showMore: function() {
         App.pageController.set('errorMessage', '');
         this.gotoState('showingMore');
+      },
+      
+      showDialog: function(recordIndex) {
+        recordIndex = parseInt(recordIndex);
+        App.pageController.selectRecord(recordIndex);
+        this.gotoState('showingDialog');
+      }
+    }),
+
+    showingDialog: SC.State.extend({
+      enterState: function() {
+        $('#userDialog').dialog('open')
+      },
+
+      exitState: function() {
+        App.pageController.deselectRecord();
+        $('#userDialog').dialog('close')
+      },
+
+      hideDialog: function() {
+        this.gotoState('notSearching');
+      },
+
+      showPreviousRecord: function() {
+        var recordIndex = App.pageController.get('selectedRecordIndex');
+        if (recordIndex === 0 ) {
+          return;
+        }
+
+        // Discard selected record
+        App.pageController.deselectRecord();
+
+        // Show previous
+        recordIndex = recordIndex - 1;
+        App.pageController.selectRecord(recordIndex);
+      },
+
+      showNextRecord: function() {
+        var recordIndex = App.pageController.get('selectedRecordIndex');
+        if (recordIndex === App.resultsController.get('length') - 1) {
+          return;
+        }
+
+        // Discard selected record
+        App.pageController.deselectRecord();
+
+        // Show next
+        recordIndex = recordIndex + 1;
+        App.pageController.selectRecord(recordIndex);
       }
     }),
 
