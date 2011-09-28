a4738cb8b26f51563d889e48d07449c2820182b1
Veebs <vibuli@gmail.com>
2011-09-28 10:34:30 +1000

    Updated windows service installer

--- src/bin/windows_service_install.bat
+++ src/bin/windows_service_install.bat
@@ -1,6 +1,27 @@
 @ echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
 
+rem ***************************************************************************************
+rem This bat file installs ChiliLog Server as a windows service
+rem 
+rem USAGE: windows_service_install.bat [32|64|ia64]
+rem        - 32 = 32 bit Windows and 32 bit java run time
+rem        - 64 = 64 bit Windows with Intel or AMD processor and 64 bit java run time
+rem        - ia64 = 64 bit Windows with Itanium processor and 64 bit java run time
+rem ***************************************************************************************
+
+rem check for parameters
+if [%1]==[] goto usage
+goto validParameters
+
+:usage
+echo USAGE: windows_service_install.bat [32 ^| 64 ^| ia64]
+echo        - 32 = 32 bit Windows and 32 bit java run time
+echo        - 64 = 64 bit Windows with Intel or AMD processor and 64 bit java run time
+echo        - ia64 = 64 bit Windows with Itanium processor and 64 bit java run time
+goto end
+
+:validParameters
 rem Change directory to the directory where the script is located
 cd /d %~dp0
 cd ..
@@ -17,11 +38,12 @@ set "CLASSPATH=.;%CHILILOG_HOME%\config"
 for /R %CHILILOG_HOME%\lib %%A in (*.jar) do (
     set CLASSPATH=!CLASSPATH!;%%A
 )
+
 :gotHome
 rem Check if the apache daemon program exists
-set "EXECUTABLE=%CHILILOG_HOME%\bin\ChiliLogServer.exe"
+set "EXECUTABLE=%CHILILOG_HOME%\bin\ChiliLogServer%1.exe"
 if exist "%EXECUTABLE%" goto okHome
-echo The ChiliLogServer.exe was not found...
+echo "The ChiliLogServer%1.exe was not found..."
 echo The CHILILOG_HOME environment variable is not defined correctly.
 echo This environment variable is needed to run this program
 goto end
@@ -65,7 +87,9 @@ set PR_DISPLAYNAME=ChiliLog Server
 set PR_JVMMX=512
 set PR_STARTMODE=jvm
 set PR_STOPMODE=jvm
-set PR_STOPTIMEOUT=5
+set PR_STOPTIMEOUT=7
+set "PR_STARTPATH=%CHILILOG_HOME%\bin"
+set "PR_STOPPATH=%CHILILOG_HOME%\bin"
 set "PR_INSTALL=%EXECUTABLE%"
 set "PR_LOGPATH=%CHILILOG_HOME%\logs"
 set PR_STDOUTPUT=auto
--- src/bin/windows_service_uninstall.bat
+++ src/bin/windows_service_uninstall.bat
@@ -1,6 +1,27 @@
 @ echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
 
+rem ***************************************************************************************
+rem This bat file uninstalls ChiliLog Server as a windows service
+rem 
+rem USAGE: windows_service_uninstall.bat [32|64|ia64]
+rem        - 32 = 32 bit Windows and 32 bit java run time
+rem        - 64 = 64 bit Windows with Intel or AMD processor and 64 bit java run time
+rem        - ia64 = 64 bit Windows with Itanium processor and 64 bit java run time
+rem ***************************************************************************************
+
+rem check for parameters
+if [%1]==[] goto usage
+goto validParameters
+
+:usage
+echo USAGE: windows_service_uninstall.bat [32 ^| 64 ^| ia64]
+echo        - 32 = 32 bit Windows and 32 bit java run time
+echo        - 64 = 64 bit Windows with Intel or AMD processor and 64 bit java run time
+echo        - ia64 = 64 bit Windows with Itanium processor and 64 bit java run time
+goto end
+
+:validParameters
 rem Change directory to the directory where the script is located
 cd /d %~dp0
 cd ..
@@ -10,9 +31,9 @@ set SERVICE_NAME=ChiliLogServer
 set "CHILILOG_HOME=%cd%"
 
 rem Check if the apache daemon program exists
-set "EXECUTABLE=%CHILILOG_HOME%\bin\ChiliLogServer.exe"
+set "EXECUTABLE=%CHILILOG_HOME%\bin\ChiliLogServer%1.exe"
 if exist "%EXECUTABLE%" goto doUninstall
-echo The ChiliLogServer.exe was not found...
+echo "The ChiliLogServer%1.exe was not found..."
 echo The CHILILOG_HOME environment variable is not defined correctly.
 echo This environment variable is needed to run this program
 goto end
