f41b009fe96df28fa3b4e66d07282358ef79ee91
Veebs <vibuli@gmail.com>
2011-09-30 21:28:32 +1000

    Fixed bad strings.

---
+++ src/main/java/org/chililog/server/pubsub/websocket/package-info.java
@@ -0,0 +1,24 @@
+//
+// Copyright 2011 Cinch Logic Pty Ltd.
+//
+// http://www.chililog.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+/**
+ * This package has been submitted to netty.  See https://github.com/netty/netty/pull/26.
+ * If and when the pull request is accepted, this package will be removed and the netty
+ * package used.
+ */
+package org.chililog.server.pubsub.websocket;
